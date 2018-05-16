afac32ae40869518030d24efa75bfc412b0d75e7
TAG <charliematina@gmail.com>
2018-05-17 11:30:51 +1200

    added favicon

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="Arival">
-		<!-- <link rel="shortcut icon" type="image/png" href="/favicon.png"/> -->
+		<link rel="shortcut icon" type="image/png" href="/favicon.png"/>
 		<title>Arival</title>
 		<link rel="stylesheet" href="https://use.typekit.net/heh7zye.css">
 		<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
