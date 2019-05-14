c373cf5abf23cc09f8883eff8b086695099b475d
Colin Matcham <cmatcham@gmail.com>
2019-05-14 16:17:49 +1200

    Fix advisors - remove Ted, add Matt

--- team.php
+++ team.php
@@ -100,22 +100,22 @@
 		<div class="col-10 col-sm-6 col-md-3 d-flex flex-wrap pb-5">
 			<div class="col-12 pb-3 d-flex justify-content-center pb-4">
 				<div class="circle-container">
-					<img src="img/tc.jpg" alt="">
+					<img src="img/rg.jpg" alt="">
 				</div>
 			</div>
 			<div class="col-12 text-center justify-content-center pb-2">
-				<h3 class="ls-4 mb-2">TED COHEN</h3>
+				<h3 class="ls-4 mb-2">ROHAN GOPALDAS</h3>
 				<p>Advisor</p>
 			</div>
 		</div>
 		<div class="col-10 col-sm-6 col-md-3 d-flex flex-wrap pb-5">
 			<div class="col-12 pb-3 d-flex justify-content-center pb-4">
 				<div class="circle-container">
-					<img src="img/rg.jpg" alt="">
+					<img src="img/mc.jpg" alt="">
 				</div>
 			</div>
 			<div class="col-12 text-center justify-content-center pb-2">
-				<h3 class="ls-4 mb-2">ROHAN GOPALDAS</h3>
+				<h3 class="ls-4 mb-2">MATT COLEMAN</h3>
 				<p>Advisor</p>
 			</div>
 		</div>
