1ea65c4b3af90ab53e43ed425b3b3bdd3c8dcbd3
Colin Matcham <cmatcham@gmail.com>
2019-05-14 16:11:17 +1200

    Remove Sarah from advisors

--- team.php
+++ team.php
@@ -100,17 +100,6 @@
 		<div class="col-10 col-sm-6 col-md-3 d-flex flex-wrap pb-5">
 			<div class="col-12 pb-3 d-flex justify-content-center pb-4">
 				<div class="circle-container">
-					<img src="img/sk.jpg" alt="">
-				</div>
-			</div>
-			<div class="col-12 text-center justify-content-center pb-2">
-				<h3 class="ls-4 mb-2">SARAH KELLY</h3>
-				<p>Advisor</p>
-			</div>
-		</div>
-		<div class="col-10 col-sm-6 col-md-3 d-flex flex-wrap pb-5">
-			<div class="col-12 pb-3 d-flex justify-content-center pb-4">
-				<div class="circle-container">
 					<img src="img/tc.jpg" alt="">
 				</div>
 			</div>
